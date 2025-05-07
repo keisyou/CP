b520c460c809d1909cbcfec32a37e71202e7555b
keisyou <shtk0llq@gmail.com>
2025-05-08 00:36:59 +0900

    AtCoder Beginner Contest 048 20250508

---
+++ AtCoder/Contest/ABC/048/20250508/A.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  string atcoder, s, contest;
+  cin >> atcoder >> s >> contest;
+
+  cout << atcoder[0] << s[0] << contest[0] << endl;
+}
---
+++ AtCoder/Contest/ABC/048/README.md
@@ -0,0 +1,5 @@
+# AtCoder Beginner Contest 048
+
+## 資料
+
+[AtCoder Beginner Contest 048](https://atcoder.jp/contests/abc048)
